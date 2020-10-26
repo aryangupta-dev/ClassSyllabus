681ecd2f8b16e657a929425f26674e56e762dddd
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-26 11:28:36 +0530

    make main class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 10/26/2020
+ *   Time: 11:14 AM
+ *   File: Main.java
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
