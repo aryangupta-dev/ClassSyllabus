9ac75d2967f6733b67fe4291a47315076832f1e3
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-26 10:28:54 +0530

    made class def multithreading

---
+++ src/MultiThreading.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 10/26/2020
+ *   Time: 10:24 AM
+ *   File: MultiThreading.java
+ */
+
+public class MultiThreading {
+
+}
