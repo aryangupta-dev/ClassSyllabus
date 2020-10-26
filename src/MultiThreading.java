db19d7830a31e18ea2868bdead180ada92179ed0
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-26 11:13:42 +0530

    override run method optimise .

--- src/MultiThreading.java
+++ src/MultiThreading.java
@@ -19,6 +19,15 @@ public class MultiThreading implements Runnable {
 
     @Override
     public void run() {
-        System.out.println();
+        System.out.println("The thread" + thread.getName() + "has started.");
+        for (int index = 0; index < limit; index++) {
+            System.out.println(thread.getName() + ": " + index);
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                System.err.print(e.getMessage());
+            }
+        }
+        System.out.println("The thread" + thread.getName() + "has closed.");
     }
 }
