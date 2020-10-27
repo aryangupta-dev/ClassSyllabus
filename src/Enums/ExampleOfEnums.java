77f9543d8eb7db0764a84e4d9e12e5e776967ac7
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-27 11:13:46 +0530

    created enum Months.java

---
+++ src/Enums/ExampleOfEnums.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 10/27/2020
+ *   Time: 11:09 AM
+ *   File: ExampleOfEnums.java
+ */
+
+package Enums;
+
+public class ExampleOfEnums {
+
+}
