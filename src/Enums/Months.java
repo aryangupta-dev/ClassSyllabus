97ecc1a7bada4489cb48a08bdeec045ede9dc535
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-27 11:31:05 +0530

    final commit in month enums

--- src/Enums/Months.java
+++ src/Enums/Months.java
@@ -1,5 +1,18 @@
 package Enums;
 
 public enum Months {
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
+    public int noOfDaysInMonth;
 
+    Months(int noOfDaysInMonth) {
+        this.noOfDaysInMonth = noOfDaysInMonth;
+    }
 }
