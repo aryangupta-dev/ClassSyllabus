daec654f9a36762fe3965509f83d7893e139e900
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-02 22:32:54 +0530

    programmed simple class to deliver the use of genrics

---
+++ src/Genrics/Main.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/2/2020
+ *   Time: 10:28 PM
+ *   File: Main.java
+ */
+
+package Genrics;
+
+public class Main {
+    public static void main(String[] args) {
+        StartGenClass<Integer> obj = new StartGenClass<Integer>();
+        obj.method1(45);
+        StartGenClass<Double> obj1 = new StartGenClass<Double>();
+        obj1.method1(45.4);
+    }
+}
