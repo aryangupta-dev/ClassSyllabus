14a2bc6ef1a1b8f5c173bf3a069d5f90a5992267
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-07 11:59:03 +0530

    Example of  Arraylist in ArrayLst.java

---
+++ src/Arraylist/ArrayLst.java
@@ -0,0 +1,16 @@
+package Arraylist;/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/7/2020
+ *   Time: 11:54 AM
+ *   File: Arraylist.ArrayLst.java
+ */
+
+import java.util.ArrayList;
+
+public class ArrayLst {
+    public static void main(String[] args) {
+        ArrayList<Integer> obj = new ArrayList<>();
+
+
+    }
+}
