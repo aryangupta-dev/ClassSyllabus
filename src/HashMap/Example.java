6fb2ad9eb5ec25dfc7014a6915213ccafefe6d2f
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-10 23:36:54 +0530

    make example to show hashmap example

---
+++ src/HashMap/Example.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/10/2020
+ *   Time: 11:23 PM
+ *   File: Example.java
+ */
+
+package HashMap;
+
+import java.util.HashMap;
+
+public class Example {
+    public static void main(String[] args) {
+        HashMap<Integer, String> studentRoll = new HashMap<>();
+
+    }
+}
