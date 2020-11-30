74bb3e6fe80e87b0b0687790befe6648351491c3
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-30 12:35:03 +0530

    add start() and end() method in the code with the while

--- src/RegexPattern/Methds.java
+++ src/RegexPattern/Methds.java
@@ -24,10 +24,24 @@ public class Methds {
          *If input is another than the no of four digits it will give false as output in line 27 sout.
          * */
         Matcher match = pattern.matcher(input);
-        System.out.println(match.matches());
-        /**
+        //System.out.println(match.matches());
+        /*
          * If find() found any subset that match with the input it will return true.
          * */
-        System.out.println(match.find());
+        /*
+         * */
+        while (match.find()) {
+            /*
+             * start will return the index of starting ,string that matches to the regular expression.*/
+            System.out.println("The start index of match " + match.start());
+            /*
+             * group() will return the string that matches first to the regular expression */
+            System.out.println("The characters were " + match.group());
+
+            /*
+             * end will return the index of end ,string that matches to the regular expression*/
+            System.out.println("The emd index of match " + match.end());
+        }
+
     }
 }
