6692bcde6ab38db19951d6b6951a2d9daf55ab45
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-12-04 13:02:02 +0530

    final commit

--- src/Stream/Lecture2nd.java
+++ src/Stream/Lecture2nd.java
@@ -8,9 +8,13 @@
 package Stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Lecture2nd {
     public static void main(String[] args) {
+        //Example for the stream on the Wrapper classes
+        //The Stream Api is bulit into the collection framework.
         ArrayList<Integer> list = new ArrayList<>();
         list.add(45);
         list.add(74);
@@ -18,6 +22,19 @@ public class Lecture2nd {
         list.add(14);
         var listStream = list.stream();
         listStream = listStream.filter(number -> number % 2 == 0).filter(number -> number > 20);
+        //filter operation (Intermdiate operation)
         listStream.forEach(number -> System.out.println(number));
+        //terminal of listStream stream u can re use it by defining it again like this
+        //listStream=list.stream();
+        Double[] carry = {1.7, 5.1, 0.5, 2.30};
+        var carryStream = Arrays.stream(carry);
+        //We can also create the stream by using the interface in java.util.stream package
+        /*1)IntStream Interface
+         * 2)LongStream Interface
+         * 3)DooubleStream Interface
+         * */
+        //Example
+        var laststream = IntStream.range(1, 100);
+        //range 1 to 99;
     }
 }
